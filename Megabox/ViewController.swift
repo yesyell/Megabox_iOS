ad1f882da3d26b6beb390c63407b4082af070cd1
yelin5262 <yelin5262@gmail.com>
2022-08-11 21:05:00 +0900

    Initial Commit

---
+++ Megabox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Megabox
+//
+//  Created by 김예린 on 2022/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
